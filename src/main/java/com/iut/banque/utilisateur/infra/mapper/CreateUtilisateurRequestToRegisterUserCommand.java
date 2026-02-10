82b982e4215f1d93d486fcad410ee6e73361dd5b
invader237 <diego.trivino.fr@gmail.com>
2026-02-10 03:30:51 +0100

    :bug:(register): fix register by adding numClient

--- src/main/java/com/iut/banque/utilisateur/infra/mapper/CreateUtilisateurRequestToRegisterUserCommand.java
+++ src/main/java/com/iut/banque/utilisateur/infra/mapper/CreateUtilisateurRequestToRegisterUserCommand.java
@@ -3,11 +3,13 @@ package com.iut.banque.utilisateur.infra.mapper;
 import com.iut.banque.model.CreateUtilisateurRequest;
 import com.iut.banque.utilisateur.domain.command.RegisterUserCommand;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.function.Function;
 
 @Mapper(componentModel = "spring")
 public interface CreateUtilisateurRequestToRegisterUserCommand extends Function<CreateUtilisateurRequest, RegisterUserCommand> {
     @Override
+    @Mapping(target = "password", source = "createUtilisateurRequest.userPwd")
     RegisterUserCommand apply(CreateUtilisateurRequest createUtilisateurRequest);
 }
